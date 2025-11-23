6a418f87895d69bf9d511ae872bc20aebe5d8c2b
Deborah Getachew <YHWH.dgse@gmail.com>
2025-11-24 00:56:03 +0300

    settings.py modified

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -41,6 +41,15 @@ INSTALLED_APPS = [
      'rest_framework.authtoken',
      'api',   
 ]
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ]
+}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
