3e14282ff5d59cfd34bc07d5ff3ea5847b76e527
Deborah <yhwh.dgse@gmail.com>
2025-11-23 18:48:18 +0300

    Add BookList and BookViewSet for CRUD operations

--- api_project /api/views.py
+++ api_project /api/views.py
@@ -1,1 +1,18 @@
+from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
+from .models import Book
+from .serializers import BookSerializer
+
+# Simple ListAPIView
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+# Full CRUD using ViewSet
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
 
