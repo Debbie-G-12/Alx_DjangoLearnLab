0583fd258de52d809ea1ae9869b5c493bae067f5
Deborah <yhwh.dgse@gmail.com>
2025-11-23 18:59:18 +0300

    Update settings.py

--- api_project /api_project/settings.py
+++ api_project /api_project/settings.py
@@ -1,3 +1,11 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = 'django-insecure-8q5(kllb8%s9_w7!c5(&nw3ho)_w(c&s00hg!w^6pzq5qjy+c#'
+DEBUG = True
+ALLOWED_HOSTS = []
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -6,9 +14,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'rest_framework',            # Django REST Framework
-    'rest_framework.authtoken',  # Token Authentication
-    'api',                       # Your app
+    'rest_framework',            
+    'rest_framework.authtoken',  
+    'api',                       
 ]
 
 REST_FRAMEWORK = {
@@ -20,3 +28,26 @@ REST_FRAMEWORK = {
     ],
 }
 
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'api_project.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
