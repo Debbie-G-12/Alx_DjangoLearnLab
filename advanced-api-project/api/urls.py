9fc67589fc6b114fe2d1539a3122ae0da12dd485
Deborah Getachew <YHWH.dgse@gmail.com>
2025-12-01 00:58:26 +0300

    Configure Book API URLs and permissions

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -4,13 +4,13 @@ from .views import (
     BookDetailView,
     BookCreateView,
     BookUpdateView,
-    BookDeleteView,
+    BookDeleteView
 )
 
 urlpatterns = [
-    path('books/', BookListView.as_view(), name='book-list'),            # list with filter/search/order
-    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'), # retrieve single book
-    path('books/create/', BookCreateView.as_view(), name='book-create'),  # create book
-    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'), # update
-    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'), # delete
+    path('books/', BookListView.as_view(), name='book-list'),
+    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
+    path('books/create/', BookCreateView.as_view(), name='book-create'),
+    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
+    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
 ]
