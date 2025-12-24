29dd6387952e87fa934395a1d9f1df6bae9ed574
Deborah <yhwh.dgse@gmail.com>
2025-12-24 22:02:58 +0300

    Add Book model to admin with custom display options
    Register the Book model with custom admin options.

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')  # Show these columns in admin
+    list_filter = ('author', 'publication_year')            # Enable filters
+    search_fields = ('title', 'author')                     # Enable search
