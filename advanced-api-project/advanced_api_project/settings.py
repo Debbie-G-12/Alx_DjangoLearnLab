7e7d3bed25e7ceed099e23731173c5fe97777f48
Deborah <yhwh.dgse@gmail.com>
2025-12-30 19:55:12 +0300

    Reorder installed apps in settings.py

--- advanced-api-project/advanced_api_project/settings.py
+++ advanced-api-project/advanced_api_project/settings.py
@@ -37,9 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "rest_framework",
-    "api",   
+    
+    'rest_framework',
+    'django_filters',
+    'api',
 ]
+
 
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': [
