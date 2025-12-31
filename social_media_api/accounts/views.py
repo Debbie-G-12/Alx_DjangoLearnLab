6e71a02c756ea9ae1aefdd87a2ddccbc3fec758b
Deborah Getachew <YHWH.dgse@gmail.com>
2025-12-31 05:16:22 +0300

    Add token auth and user profile endpoint

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+
+class ProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        return Response({
+            'username': user.username,
+            'email': user.email,
+            'bio': user.bio
+        })
 
 # Create your views here.
